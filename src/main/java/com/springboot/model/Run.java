ffc67b036916acd962d36928d9026c1db2917bf0
Tenten07 <thachapon1007@gmail.com>
2025-09-27 16:08:00 +0700

    comment in run.java

--- src/main/java/com/springboot/model/Run.java
+++ src/main/java/com/springboot/model/Run.java
@@ -4,16 +4,11 @@ import org.springframework.boot.SpringApplication;
 
 import com.springboot.SpringBootApplicationMain;
 
-/**
- * Simple launcher class for the Skill Swap application.
- *
- * Data initialization is automatically handled by DataInitializer.java
- * when the Spring Boot application starts up.
- */
+
 public class Run {
 
 	public static void main(String[] args) {
-		// Launch Spring Boot application
+		
 		SpringApplication.run(SpringBootApplicationMain.class, args);
 	}
 }
